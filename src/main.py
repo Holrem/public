c97fbefaacf2aa577e4ef079169ea02ccb5f1c3b
Holrem <c.t.grinnell@gmail.com>
2024-09-11 09:40:57 -0500

    B: Added main.sh, src/main.py and src/textnode.py

---
+++ src/main.py
@@ -0,0 +1,9 @@
+from textnode import TextNode
+
+def main():
+    # Create a TextNode object here
+    node = TextNode("This is a text node", "bold", "https://www.boot.dev")
+    print(node)
+
+if __name__ == "__main__":
+    main()
